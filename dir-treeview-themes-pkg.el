a5cee61c737c6600589fa8d96423e52ce3da525c
Tilman Rassy <tilman.rassy@googlemail.com>
2022-09-11 01:49:55 +0200

    Updated version of dependency in dir-treeview-themes-pkg.el

--- dir-treeview-themes-pkg.el
+++ dir-treeview-themes-pkg.el
@@ -2,7 +2,7 @@
   "dir-treeview-themes"
   "1.0.0"
   "Themes for dir-treeview"
-  '((emacs "24.4") (dir-treeview "1.3.0"))
+  '((emacs "24.4") (dir-treeview "1.3.1"))
    :authors '(("Tilman Rassy" . "tilman.rassy@googlemail.com"))
    :maintainer '("Tilman Rassy" . "tilman.rassy@googlemail.com")
    :keywords '("tools" "convenience" "files")
